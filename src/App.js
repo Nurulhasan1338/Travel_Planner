38abefbcfd7d8903ac1266ea3b6c718eba23df32
Nurul Hasan <2020kucp1109@iiitkota.ac.com>
2023-07-28 17:15:28 +0530

    add singnin and signup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Mysignin from "./component/signin";
+import SignUp from "./component/signup";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <Routes>
+    <Route path='/' element={<Mysignin key="login"/>} />
+    <Route path='/signup' element={<SignUp key="login"/>} />
+    </Routes>
+  </Router>
     </div>
   );
 }
